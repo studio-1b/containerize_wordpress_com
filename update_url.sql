cd5413efb239543f5ad12f54616bec693fdda178
byuan6 <47025571+byuan6@users.noreply.github.com>
2024-10-30 17:05:25 -0700

    Update update_url.sql

--- update_url.sql
+++ update_url.sql
@@ -1,4 +1,4 @@
 use DATABASE_NAME;
 update wp_options set option_value='http://hostname/' where option_name='siteurl';
 update wp_options set option_value='http://hostname/' where option_name='home';
-
+update wp_posts set post_content=replace(post_content,"http://hostname/","http://oldurl/")  where locate(post_content, CONVERT(CAST('http://oldurl/' as BINARY) USING utf8mb4))>=0;
